b10e8623a8281977dbd970c2e7a6321715f169e0
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-06 22:24:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,6 +28,10 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
   
+  def self.create
+    
+  end
+  
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
