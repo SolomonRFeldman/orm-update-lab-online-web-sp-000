79ed7956fc8016d779aba1b2fd8ffbed53ee50cb
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-06 23:01:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,6 @@ class Student
   
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE students.name = ? LIMIT 1"
-    binding.pry
     Student.new_from_db(DB[:conn].execute(sql, name).first)
   end
   
